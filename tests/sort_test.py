34178b7c93c561e1dee1921694affd35f02dae08
Kang Hyojun <hyojun@admire.kr>
2015-05-01 22:31:08 +0900

    Fix `None` cannot be orderable in python3.x
      - close #9

--- tests/sort_test.py
+++ tests/sort_test.py
@@ -17,3 +17,31 @@ def test_sort_relative_import_belower():
     import_names = [('hello.b', 1, 0, False), ('.a', 2, 0, True)]
     sorted_ = sort_import_names(import_names, local_package_names)
     assert import_names == sorted_
+
+
+def test_sort_constant():
+    local_package_names = ['hello']
+    import_names = [('.FOO', 1, 0, True), ('.foo', 2, 0, True)]
+    sorted_ = sort_import_names(import_names, local_package_names)
+    assert import_names == sorted_
+
+
+def test_sort_class_name():
+    local_package_names = ['hello']
+    import_names = [('.FooClass', 1, 0, True), ('.foo', 2, 0, True)]
+    sorted_ = sort_import_names(import_names, local_package_names)
+    assert import_names == sorted_
+
+
+def test_sort_all():
+    local_package_names = ['hello']
+    import_names = [
+        ('re.compile', 1, 0, False),
+        ('pygments.highlight', 2, 0, False),
+        ('hello.FOO', 3, 0, False),
+        ('hello.foo', 3, 0, False),
+        ('.FooClass', 4, 0, True),
+        ('.a', 4, 0, True),
+    ]
+    sorted_ = sort_import_names(import_names, local_package_names)
+    assert import_names == sorted_
