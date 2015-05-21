8421731d771c69e8ab8e80daec9768aea1c599b8
Yeonjun Lim <yjroot@gmail.com>
2015-05-21 10:23:25 +0900

    Change the use of `str.format`

--- import_order/filter.py
+++ import_order/filter.py
@@ -5,7 +5,7 @@ class Exclude:
     def __init__(self, excludes):
         excludes = [os.path.abspath(path) for path in excludes]
         self.file_excluds = [path for path in excludes if os.path.isfile(path)]
-        self.dir_excluds = [str.format('{path}{sep}', path=path, sep=os.sep)
+        self.dir_excluds = ['{path}{sep}'.format(path=path, sep=os.sep)
                             for path in excludes if os.path.isdir(path)]
 
     def __call__(self, files):
